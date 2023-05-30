e047f8097022cdec6ada86b64f96504af83cf820
AliElgohary <alielgohary35@gmail.com>
2023-05-30 11:36:59 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,41 @@
+function addTask() {
+    let taskName = document.getElementById("newTask").value;
+    let priority = document.getElementById("priority").value;
+    let taskNumber = taskCount;
+    let task = new Task(taskNumber, taskName, priority);
+    tasks.push(task);
+    document.querySelector("table").innerHTML += `
+          <tr id="${task.taskNumber}">  
+          <td scope="col">${taskCount}</td>
+          <td scope="col">${task.taskName}</td>
+          <td scope="col">${task.priority}</td>
+          <td scope="col"><button class="btn btn-success" id="delete" onclick="deleteTask(${task.taskNumber})" >delete</button></td>
+          </tr>
+          `;
+    taskCount++;
+  }
+  function deleteTask(taskNumber) {
+    tasks.splice(tasks.indexOf(tasks.find(task=> task.taskNumber == taskNumber)), 1);
+    console.log(tasks);
+    document.getElementById(`${taskNumber}`).remove();
+  }
+  
+  let taskCount = 1;
+  const tasks = [];
+  
+  document.getElementById("add").onclick = function () {
+    if (document.getElementById("newTask").value.length == 0) {
+      alert("please input a valid task");
+    } else {
+      addTask();
+    }
+  };
+  
+  
+  class Task {
+    constructor(taskNumber,taskName, priority) {
+      this.taskName = taskName;
+      this.priority = priority;
+      this.taskNumber = taskNumber;
+    }
+  }
