45c6283f9ca2b7df011cfa2578027593aae8f8b6
AliElgohary <alielgohary35@gmail.com>
2023-06-01 04:37:14 +0300

    more organized and better functioning

--- script.js
+++ script.js
@@ -1,47 +1,56 @@
 class Task {
-  constructor(taskNumber, taskName, priority) {
+  constructor(taskName, priority) {
     this.taskName = taskName;
     this.priority = priority;
-    this.taskNumber = taskNumber;
   }
 }
-function addTask() {
-  let taskName = document.getElementById("newTask").value;
-  let priority = document.getElementById("priority").value;
-  let taskNumber = taskCount;
-  let task = new Task(taskNumber, taskName, priority);
-  tasks.push(task);
-  document.querySelector("table").innerHTML += `
-          <tr id="${task.taskNumber}">  
-          <td scope="col">${taskCount}</td>
-          <td scope="col">${task.taskName}</td>
-          <td scope="col">${task.priority}</td>
-          <td scope="col"><button class="btn btn-danger" id="delete" onclick="deleteTask(${task.taskNumber})" >delete</button></td>
-          </tr>
-          `;
-  taskCount++;
-}
-function deleteTask(taskNumber) {
-  tasks.splice(
-    tasks.indexOf(tasks.find((task) => task.taskNumber == taskNumber)),
-    1
-  );
-  console.log(tasks);
-  document.getElementById(`${taskNumber}`).remove();
-}
-
 let taskCount = 1;
 const tasks = [];
 
-document.getElementById("add").onclick = function () {
+const addTask = function () {
+  const taskName = document.getElementById("newTask").value;
+  const priority = document.getElementById("priority").value;
+  const task = new Task(taskName, priority);
+  tasks.push(task);
+};
+
+document.getElementById("add").onclick = function (task) {
   if (document.getElementById("newTask").value.length == 0) {
     alert("please input a valid task");
-  }else if (document.getElementById("priority").value.length == 0){
-    alert("please input a valid priority (low, med, or high)");
-  }else if (document.getElementById("priority").value !== "low" && document.getElementById("priority").value !== "med" && document.getElementById("priority").value !== "high") {
-    alert("please input a valid priority (low, med, or high)");
-  } 
-  else {
+  } else if (
+    isNaN(Number(document.getElementById("priority").value)) ||
+    Number(document.getElementById("priority").value) < 1 ||
+    Number(document.getElementById("priority").value) > 5
+  ) {
+    alert("please input a valid priority, number between 1 and 5.");
+  } else {
     addTask();
+    renderTaskTable();
+    taskCount++;
   }
+};
+
+const addTaskToList = function (i, task) {
+  let tr = `
+          <tr">  
+          <td scope="col">${i + 1}</td>
+          <td scope="col">${task.taskName}</td>
+          <td scope="col">${task.priority}</td>
+          <td scope="col"><button class="btn btn-danger" id="delete" onclick="deleteTask(${i})" >delete</button></td>
+          </tr>
+          `;
+
+  return tr;
+};
+const renderTaskTable = function () {
+  let tbody = "";
+  for (let i = 0; i < tasks.length; i++) {
+    tbody += addTaskToList(i, tasks[i]);
+  }
+  document.querySelector("table").innerHTML = tbody;
+};
+
+const deleteTask = function (i) {
+  tasks.splice(i, 1);
+  renderTaskTable();
 };
